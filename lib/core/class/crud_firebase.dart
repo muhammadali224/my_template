91dfa18f8c4cda3fafbc14d40f491abb94b5af54
muhammad ali <muhammad.alfaqoui@gmail.com>
2023-08-21 01:39:25 +0300

    change performance for crud_firebase.dart

--- lib/core/class/crud_firebase.dart
+++ lib/core/class/crud_firebase.dart
@@ -1,95 +1,204 @@
+// import 'dart:io';
+//
 // import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:get/get.dart';
 //
-import 'response.dart';
-
+// import '../services/services.dart';
+// import 'custom_response.dart' as response_dart;
 //
-// final FirebaseFirestore firestore = FirebaseFirestore.instance;
+// MyServices myServices = Get.find();
 //
-class CRUDFirebase {
-  static Response response = Response();
-  static Future<Response> addData({
-    required String collectionName,
-    required Map<String, dynamic> data,
-  }) async {
+// class CRUDFirebase {
+//   static response_dart.CustomResponse myResponse =
+//   response_dart.CustomResponse();
+//
+//   static var firestore = myServices.firestore;
+//   static var fireStorage = myServices.fireStorage;
+//
+//   static Future<(response_dart.CustomResponse, String)> addData({
+//     required String collectionName,
+//     required Map<String, dynamic> data,
+//     String? path,
+//     String? subCollection,
+//   }) async {
 //     CollectionReference collectionReference =
-//         firestore.collection(collectionName);
-//     DocumentReference documentReferencer = collectionReference.doc();
+//     firestore.collection(collectionName);
+//
+//     DocumentReference documentReferencer = subCollection == null
+//         ? collectionReference.doc(path)
+//         : collectionReference.doc(path).collection(subCollection).doc();
 //     await documentReferencer.set(data).whenComplete(() {
-//       response.code = 200;
-//       response.message = "success";
-//       updateData(
-//           collectionName: collectionName,
-//           docId: documentReferencer.id,
-//           data: {'id': documentReferencer.id});
+//       myResponse.code = 200;
+//       myResponse.message = "success";
+//
+//       documentReferencer.update({'id': documentReferencer.id});
 //     }).catchError((e) {
-//       response.code = 500;
-//       response.message = e;
+//       myResponse.code = 500;
+//       myResponse.message = e.toString();
 //     });
 //     print(documentReferencer.id);
-    return response;
-  }
+//     return (myResponse, documentReferencer.id);
+//   }
 //
 //   static Stream<QuerySnapshot> readData({required String collectionName}) {
 //     CollectionReference notesItemCollection =
-//         firestore.collection(collectionName);
+//     firestore.collection(collectionName);
 //
 //     return notesItemCollection.snapshots();
 //   }
-
-  // static Future<(List<QueryDocumentSnapshot<Object?>>, Response)> getData({
-  //   required String collectionName,
-  // }) async {
-  //   CollectionReference collectionReference =
-  //   firestore.collection(collectionName);
-  //   QuerySnapshot query = await collectionReference.get().whenComplete(() {
-  //     response.code = 200;
-  //     response.message = "success";
-  //   });
-  //   if (query.docs.isEmpty) {
-  //     response.code = 500;
-  //     response.message = "empty";
-  //   }
-  //   return (query.docs, response);
-  // }
-
 //
-//   static Future<Response> updateData({
+//   static Future<
+//       (List<QueryDocumentSnapshot<Object?>>, response_dart.CustomResponse)>
+//   getData({
 //     required String collectionName,
-//     required String docId,
-//     required Map<String, dynamic> data,
+//     String? subCollection,
+//     String? subDocument,
+//     String? orderBy,
+//     bool? ascending,
 //   }) async {
-//     Response response = Response();
-//     CollectionReference collectionReference =
-//         firestore.collection(collectionName);
-//     DocumentReference documentReferencer = collectionReference.doc(docId);
+//     CollectionReference collectionReference = subCollection == null
+//         ? firestore.collection(collectionName)
+//         : firestore
+//         .collection(collectionName)
+//         .doc(subDocument)
+//         .collection(subCollection);
+//     QuerySnapshot query;
+//     if (orderBy != null) {
+//       query = await collectionReference
+//           .orderBy(orderBy, descending: ascending ?? false)
+//           .get()
+//           .whenComplete(() {
+//         myResponse.code = 200;
+//         myResponse.message = "success";
+//       });
+//     } else {
+//       query = await collectionReference.get().whenComplete(() {
+//         myResponse.code = 200;
+//         myResponse.message = "success";
+//       });
+//     }
 //
-//     await documentReferencer.update(data).whenComplete(() {
-//       response.code = 200;
-//       response.message = "success";
-//     }).catchError((e) {
-//       response.code = 500;
-//       response.message = e;
-//     });
-//     return response;
+//     if (query.docs.isEmpty) {
+//       myResponse.code = 500;
+//       myResponse.message = "empty";
+//     }
+//     return (query.docs, myResponse);
 //   }
 //
-//   static Future<Response> deleteData({
+//   static Future<
+//       (List<QueryDocumentSnapshot<Object?>>, response_dart.CustomResponse)>
+//   getQueryData({
+//     required String collectionName,
+//     String? orderBy,
+//     bool? ascending,
+//     required String field,
+//     required String condition,
+//   }) async {
+//     CollectionReference collectionReference =
+//     firestore.collection(collectionName);
+//     QuerySnapshot query;
+//     if (orderBy != null) {
+//       query = await collectionReference
+//           .orderBy(orderBy, descending: ascending ?? false)
+//           .where(field, isEqualTo: condition)
+//           .get()
+//           .whenComplete(() {
+//         myResponse.code = 200;
+//         myResponse.message = "success";
+//       });
+//     } else {
+//       query = await collectionReference
+//           .where(field, isEqualTo: condition)
+//           .get()
+//           .whenComplete(() {
+//         myResponse.code = 200;
+//         myResponse.message = "success";
+//       });
+//     }
+//
+//     if (query.docs.isEmpty) {
+//       myResponse.code = 500;
+//       myResponse.message = "empty";
+//     }
+//     return (query.docs, myResponse);
+//   }
+//
+//   static Future<response_dart.CustomResponse> updateData({
 //     required String collectionName,
 //     required String docId,
+//     String? subCollectionName,
+//     String? subCollectionId,
 //     required Map<String, dynamic> data,
 //   }) async {
-//     Response response = Response();
 //     CollectionReference collectionReference =
-//         firestore.collection(collectionName);
+//     firestore.collection(collectionName);
+//     DocumentReference documentReferencer = subCollectionName == null
+//         ? collectionReference.doc(docId)
+//         : collectionReference
+//         .doc(docId)
+//         .collection(subCollectionName)
+//         .doc(subCollectionId);
+//
+//     await documentReferencer.update(data).whenComplete(() {
+//       myResponse.code = 200;
+//       myResponse.message = "success";
+//     }).catchError((e) {
+//       myResponse.code = 500;
+//       myResponse.message = e.toString();
+//     });
+//     return myResponse;
+//   }
+//
+//   static Future<response_dart.CustomResponse> deleteData({
+//     required String collectionName,
+//     required String docId,
+//   }) async {
+//     CollectionReference collectionReference =
+//     firestore.collection(collectionName);
 //     DocumentReference documentReferencer = collectionReference.doc(docId);
 //
 //     await documentReferencer.delete().whenComplete(() {
-//       response.code = 200;
-//       response.message = "success";
+//       myResponse.code = 200;
+//       myResponse.message = "success";
 //     }).catchError((e) {
-//       response.code = 500;
-//       response.message = e;
+//       myResponse.code = 500;
+//       myResponse.message = e.toString();
 //     });
-//     return response;
+//     return myResponse;
 //   }
-}
+//
+//   static Future<response_dart.CustomResponse> deleteFile({
+//     required String collectionName,
+//     required String docId,
+//   }) async {
+//     Reference ref =
+//     fireStorage.ref().child(collectionName).child(docId).child(docId);
+//
+//     await ref.delete().whenComplete(() {
+//       myResponse.code = 200;
+//       myResponse.message = "success";
+//     }).catchError((e) {
+//       myResponse.code = 500;
+//       myResponse.message = e.toString();
+//     });
+//     return myResponse;
+//   }
+//
+//   static Future<(String, response_dart.CustomResponse)> uploadFile({
+//     required File file,
+//     required String childName,
+//     required String id,
+//     required String nameId,
+//   }) async {
+//     String downloadURL;
+//     Reference ref = fireStorage.ref().child(childName).child(id).child(nameId);
+//     await ref.putFile(file).whenComplete(() {
+//       myResponse.code = 200;
+//       myResponse.message = "success";
+//     });
+//     downloadURL = await ref.getDownloadURL();
+//
+//     return (downloadURL, myResponse);
+//   }
+// }
